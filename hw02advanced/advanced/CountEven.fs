c26cb159d99f6c814ba32d7013ec89db9ebc2181
konstantin.ovsyannikov <st107930@gmail.com>
2024-03-04 00:32:04 +0300

    add: even counter task

---
+++ hw02advanced/advanced/CountEven.fs
@@ -0,0 +1,13 @@
+﻿let listCountEvenMap list =
+    list |> List.map (fun x -> (x + 1) % 2) |> List.sum
+
+let listCountEvenFilter list =
+    list |> List.filter (fun x -> x % 2 = 0) |> List.length
+
+let listCountEvenFold list =
+    list |> List.fold (fun acc x -> acc + (x + 1) % 2) 0
+
+let list = [ 1; 2; 3; 4; 5 ]
+printfn "%A" <| listCountEvenMap list
+printfn "%A" <| listCountEvenFilter list
+printfn "%A" <| listCountEvenFold list
